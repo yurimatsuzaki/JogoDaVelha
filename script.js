41d61f2a6af2c2f3c3303aabf9ee87210cac7750
yurimatsuzaki <155598762+yurimatsuzaki@users.noreply.github.com>
2024-08-30 00:14:14 -0300

    adicionando logica e recursos

---
+++ script.js
@@ -0,0 +1,153 @@
+var controlhe = []
+var jogador = 0
+
+        function cor(id){
+            let quadrados = document.getElementsByClassName('quad')
+            for(let  i=0; i>9; i++){
+                quadrados.style
+            }
+        }
+
+        function jogada00(){
+            let quadrado1 = document.getElementById('0-0')
+            
+            if(jogador == 0){
+                quadrado1.style.backgroundColor='yellow'
+                jogador = 1
+                controlhe += 1
+                console.log(`${controlhe}`)
+            }else{
+                quadrado1.style.backgroundColor='blue'
+                jogador = 0
+                controlhe += 0
+                console.log(`${controlhe}`)
+            }
+        }
+
+        function jogada01(){
+            let quadrado2 = document.getElementById('0-1')
+            
+            if(jogador == 0){
+                quadrado2.style.backgroundColor='yellow'
+                jogador = 1
+                controlhe += 1
+                console.log(`${controlhe}`)
+            }else{
+                quadrado2.style.backgroundColor='blue'
+                jogador = 0
+                controlhe += 0
+                console.log(`${controlhe}`)
+            }
+        }
+
+        function jogada02(){
+            let quadrado3 = document.getElementById('0-2')
+            
+            if(jogador == 0){
+                quadrado3.style.backgroundColor='yellow'
+                jogador = 1
+                controlhe += 1
+                console.log(`${controlhe}`)
+            }else{
+                quadrado3.style.backgroundColor='blue'
+                jogador = 0
+                controlhe += 0
+                console.log(`${controlhe}`)
+            }
+        }
+
+        function jogada10(){
+            let quadrado4 = document.getElementById('1-0')
+            
+            if(jogador == 0){
+                quadrado4.style.backgroundColor='yellow'
+                jogador = 1
+                controlhe += 1
+                console.log(`${controlhe}`)
+            }else{
+                quadrado4.style.backgroundColor='blue'
+                jogador = 0
+                controlhe += 0
+                console.log(`${controlhe}`)
+            }
+        }
+
+        function jogada11(){
+            let quadrado5 = document.getElementById('1-1')
+            
+            if(jogador == 0){
+                quadrado5.style.backgroundColor='yellow'
+                jogador = 1
+                controlhe += 1
+                console.log(`${controlhe}`)
+            }else{
+                quadrado5.style.backgroundColor='blue'
+                jogador = 0
+                controlhe += 0
+                console.log(`${controlhe}`)
+            }
+        }
+
+        function jogada12(){
+            let quadrado6 = document.getElementById('1-2')
+            
+            if(jogador == 0){
+                quadrado6.style.backgroundColor='yellow'
+                jogador = 1
+                controlhe += 1
+                console.log(`${controlhe}`)
+            }else{
+                quadrado6.style.backgroundColor='blue'
+                jogador = 0
+                controlhe += 0
+                console.log(`${controlhe}`)
+            }
+        }
+
+        function jogada20(){
+            let quadrado7 = document.getElementById('2-0')
+            
+            if(jogador == 0){
+                quadrado7.style.backgroundColor='yellow'
+                jogador = 1
+                controlhe += 1
+                console.log(`${controlhe}`)
+            }else{
+                quadrado7.style.backgroundColor='blue'
+                jogador = 0
+                controlhe += 0
+                console.log(`${controlhe}`)
+            }
+        }
+
+        function jogada21(){
+            let quadrado8 = document.getElementById('2-1')
+            
+            if(jogador == 0){
+                quadrado8.style.backgroundColor='yellow'
+                jogador = 1
+                controlhe += 1
+                console.log(`${controlhe}`)
+            }else{
+                quadrado8.style.backgroundColor='blue'
+                jogador = 0
+                controlhe += 0
+                console.log(`${controlhe}`)
+            }
+        }
+
+        function jogada22(){
+            let quadrado9 = document.getElementById('2-2')
+            
+            if(jogador == 0){
+                quadrado9.style.backgroundColor='yellow'
+                jogador = 1
+                controlhe += 1
+                console.log(`${controlhe}`)
+            }else{
+                quadrado9.style.backgroundColor='blue'
+                jogador = 0
+                controlhe += 0
+                console.log(`${controlhe}`)
+            }
+        }
